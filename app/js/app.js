939041273056c59f7755c2f994af3283e52b2387
Pedro Howat <pedro.howat@gmail.com>
2013-01-26 18:40:10 -0200

    Small refactor.

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,13 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: HomeCtrl});
-    $routeProvider.when('/session/:sessType/:token', {templateUrl: 'partials/session.html', controller: SessionCtrl});
+    $routeProvider.when(
+        '/home', 
+        {templateUrl: 'partials/home.html', controller: HomeCtrl}
+    );
+    $routeProvider.when(
+        '/session/:sessType/:token', 
+        {templateUrl: 'partials/session.html', controller: SessionCtrl}
+    );
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
